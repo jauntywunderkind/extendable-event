d38c5d0b889ec5979c78fee784b592f1f93c0ccf
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-29 15:03:21 -0400

    meh add pendingCount to see what it looks like

--- extendable-event.js
+++ extendable-event.js
@@ -2,30 +2,43 @@
 
 export const
   $lifetimes= Symbol.for( "extendable-event:extend-lifetime-promises"),
-  $watched= Symbol.for( "extendable-event:watched")
+  // these are all for "pendingCount":
+  $watched= Symbol.for( "extendable-event:watched"),
+  $resolved= Symbol.for( "extendable-event:resolved"),
+  $watcher= Symbol.for( "extendable-event:watcher")
 
 export function ExtendableEvent(){
-	this[ lifetimes]= []
+	this[ $lifetimes]= []
 	return this
 }
 export default ExtendableEvent
 
 ExtendableEvent.prototype.waitUntil= function( promise){
-	this[ lifetimes].push( promise)
+	this[ $lifetimes].push( promise)
 }
 ExtendableEvent.prototype.then= async function(){
 	while( true){
 		const
-		  extenders= this[ lifetimes]
-		  originalCount= this[ lifetimes].length
-		await Promise.all( extenders]
+		  extenders= this[ $lifetimes]
+		  originalCount= this[ $lifetimes].length
+		await Promise.all( this[ $lifetimes])
 
 		// re-run if there are new lifetimes added
-		if( this[ lifetimes].length=== originalCount){
+		if( this[ $lifetimes].length=== originalCount){
 			break
 		}
 	}
 }
+ExtendableEvent.prototype.pendingCount= async function(){
+	const watcher= this[ $watcher]|| (this[ $watcher]= ()=> ++this[ $resolved])
+	for( let i= this[ $watched]|| 0; i< this[ $lifetimes].length; ++i){
+		this[ $lifetimes].then( watcher)
+	}
+	this[ $watched]= this[ $lifetimes].length= 0
+	await delay()
+	return this[ $lifetimes].length- this[ $resolved]
+}
+
 ExtendableEvent.mixin= function( klass){
 }
 
